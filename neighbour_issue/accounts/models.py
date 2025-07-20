9eae9fbe78677d571f565f6edc038c3a26065614
sekibaalamark <markmajje256@gmail.com>
2025-07-20 19:01:55 +0300

    updated my user model

--- neighbour_issue/accounts/models.py
+++ neighbour_issue/accounts/models.py
@@ -10,6 +10,8 @@ class CustomUser(AbstractUser):
         ('NEMA', 'NEMA'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='resident')
+    username = models.CharField(unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.role
+        return self.username
