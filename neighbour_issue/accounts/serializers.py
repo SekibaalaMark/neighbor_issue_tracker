d458a8c8404082f76d52f54da43497e755009559
sekibaalamark <markmajje256@gmail.com>
2025-07-21 19:13:10 +0300

    created a serializer to send an email confirmation link to users during registration

--- neighbour_issue/accounts/serializers.py
+++ neighbour_issue/accounts/serializers.py
@@ -1,4 +1,7 @@
+# serializers.py
 from rest_framework import serializers
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 
@@ -14,6 +17,17 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             role=validated_data['role'],
-            password=validated_data['password']
+            password=validated_data['password'],
         )
+
+        # Send verification email
+        verification_link = f"http://localhost:8000/api/verify-email/{user.verification_code}/"
+
+        send_mail(
+            subject='Verify your email',
+            message=f'Hi {user.username},\n\nClick the link to verify your email: {verification_link}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+        )
+
         return user
