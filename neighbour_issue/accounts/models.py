00f5b9a148081ba5e0387e0235c983c2a58a0560
sekibaalamark <markmajje256@gmail.com>
2025-07-21 19:12:34 +0300

    updated my user model such that the accounts become inactive by default for email confirmation

--- neighbour_issue/accounts/models.py
+++ neighbour_issue/accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+# models.py
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
@@ -9,9 +11,13 @@ class CustomUser(AbstractUser):
         ('UNRA', 'UNRA'),
         ('NEMA', 'NEMA'),
     )
+
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='resident')
-    username = models.CharField(unique=True)
+    username = models.CharField(unique=True, max_length=150)
     email = models.EmailField(unique=True)
+    
+    is_verified = models.BooleanField(default=False)  # 👈 Add this
+    verification_code = models.UUIDField(default=uuid.uuid4)  # 👈 Add this
 
     def __str__(self):
         return self.username
